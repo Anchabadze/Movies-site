0d3b19cddeaa550fd3952f0d78b5a54d75570f93
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-09 22:45:51 -0300

    Movies list JS added

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@
 
 const adv = document.querySelectorAll('.promo__adv img'),
         poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector('.promo__genre');
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list');
 
 
 adv.forEach(item => {
@@ -19,3 +20,18 @@ adv.forEach(item => {
 });
 
 genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
