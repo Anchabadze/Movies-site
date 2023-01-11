d23956fe701acdcaab76757d7ff58750f0c7bdf0
Zurab Anchabadze <zurab.anchabadze@gmail.com>
2023-01-10 22:00:29 -0300

    Input form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
- const movieDB = {
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
     movies: [
         "Логан",
         "Лига справедливости",
@@ -7,31 +8,41 @@
         "Одержимость",
         "Скотт Пилигрим против..."
     ]
-};
+    };
 
-const adv = document.querySelectorAll('.promo__adv img'),
-        poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector('.promo__genre'),
-        movieList = document.querySelector('.promo__interactive-list');
+    const adv = document.querySelectorAll('.promo__adv img'),
+            poster = document.querySelector('.promo__bg'),
+            genre = poster.querySelector('.promo__genre'),
+            movieList = document.querySelector('.promo__interactive-list'),
+            addForm = document.querySelector('form.add'),
+            addInput = addForm.querySelector('.adding__input'),
+            checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (e) => { // 23:32
+        e.preventDefault();
+    });
+
+    adv.forEach(item => {
+        item.remove();
+    });
+
+    genre.textContent = 'Драма';
+
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    movieList.innerHTML = '';
+
+    movieDB.movies.sort();
+
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
 
 
-adv.forEach(item => {
-    item.remove();
-});
 
-genre.textContent = 'Драма';
-
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-
-movieList.innerHTML = '';
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
 });
 
